8c302051c0c888dec27eadb03a623b5a78e71a1f
arntgm <agmalvik@gmail.com>
2016-03-02 19:33:21 -0800

    la til femten

--- app/src/main/java/com/example/masommer/mapster/MapsActivity.java
+++ app/src/main/java/com/example/masommer/mapster/MapsActivity.java
@@ -14,6 +14,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
 
+    private int femten = 15;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
