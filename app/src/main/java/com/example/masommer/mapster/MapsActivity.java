f750072379983fd1b9845960d002c02f10e48648
MaSommer <martisom@stud.ntnu.no>
2016-03-07 11:09:23 -0800

    Location, search
    Access location, search functionality

--- app/src/main/java/com/example/masommer/mapster/MapsActivity.java
+++ app/src/main/java/com/example/masommer/mapster/MapsActivity.java
@@ -1,9 +1,18 @@
 package com.example.masommer.mapster;
 
-import android.support.v4.app.FragmentActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.widget.SearchView;
 
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -14,9 +23,11 @@ import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapLoadedCallback, GoogleMap.OnMapClickListener {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapLoadedCallback, GoogleMap.OnMapClickListener, LocationListener {
 
     private GoogleMap mMap;
+    private final int MY_PERMISSION_LOCATION_ACCESS = 1;
+
 
 
     @Override
@@ -27,10 +38,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-
     }
-
 
     /**
      * Manipulates the map once available.
@@ -43,9 +51,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSION_LOCATION_ACCESS);
+        }
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
+        // Add a marker at ucsb and move the camera
         LatLng ucsb = new LatLng(34.412573495197854, -119.8470050096512);
         mMap.addMarker(new MarkerOptions().position(ucsb).title("Marker at UCSB"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(ucsb));
@@ -62,8 +77,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .zoom(15)
                 .build();
         googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cp));
+        //Set my location
+        try{
+            mMap.setMyLocationEnabled(true);
 
-
+        }
+        catch (SecurityException e){
+            e.printStackTrace();
+        }
 
     }
 
@@ -77,4 +98,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Log.i("long", ""+var1.longitude);
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+
+        // Associate searchable configuration with the SearchView
+        // SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        //SearchView searchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.action_search));
+        // searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        //searchView.setOnQueryTextListener(this);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public void onLocationChanged(Location var1){
+
+    }
+
 }
