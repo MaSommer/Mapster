686bae34798a87d89bfc3e3877087a6bce33cb88
Bendik Witzoe <bendikw@gmail.com>
2016-03-08 20:11:08 -0800

    search stuff

--- app/src/main/java/com/example/masommer/mapster/MapsActivity.java
+++ app/src/main/java/com/example/masommer/mapster/MapsActivity.java
@@ -38,12 +38,9 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-<<<<<<< HEAD
 import android.widget.AdapterView;
-=======
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
->>>>>>> refs/remotes/origin/master
 import android.widget.Toast;
 
 import com.google.android.gms.location.LocationListener;
@@ -149,13 +146,25 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     protected void onNewIntent(Intent intent) {
         handleIntent(intent);
+        //System.out.print("tried to search!");
     }
 
     private void handleIntent(Intent intent) {
 
         if (Intent.ACTION_VIEW.equals(intent.getAction())) {
             // handles a click on a search suggestion; launches activity to show word
-            Toast.makeText(getApplicationContext(), "VOILA!", Toast.LENGTH_LONG).show();
+            //System.out.print("HEIIIII");
+            String path = getIntent().getDataString();
+            Log.i("path", path);
+//            Cursor c = managedQuery(uri, null, null, null, null);
+//            c.moveToFirst();
+//            int latIndex = c.getColumnIndexOrThrow(DatabaseTable.COL_LAT);
+//            int longIndex = c.getColumnIndexOrThrow(DatabaseTable.COL_LONG);
+//            double latitude = Double.parseDouble(c.getString(latIndex));
+//            double longitude = Double.parseDouble(c.getString(longIndex));
+//            LatLng roomPoint = new LatLng(latitude,longitude);
+//            Marker room = mMap.addMarker(new MarkerOptions().position(roomPoint));
+//            Toast.makeText(getApplicationContext(), "VOILA!", Toast.LENGTH_LONG).show();
         } else if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             // handles a search query
             String query = intent.getStringExtra(SearchManager.QUERY);
@@ -177,48 +186,48 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
-    private void showResults(String query) {
-
-        Cursor cursor = new CursorLoader(getApplicationContext(),DatabaseProvider.CONTENT_URI, null, null,
-                new String[]{query}, null);
-        if (cursor == null) {
-            // There are no results
-            //mTextView.setText(getString(R.string.no_results, new Object[]{query}));
-        } else {
-            // Display the number of results
-            int count = cursor.getCount();
-            //String countString = getResources().getQuantityString(R.plurals.search_results,
-            //        count, new Object[] {count, query});
-            //mTextView.setText(countString);
-            Toast.makeText(MapsActivity.this, "WOW: You found "+count+" results!", Toast.LENGTH_SHORT).show();
-            // Specify the columns we want to display in the result
-//            String[] from = new String[] { DictionaryDatabase.KEY_WORD,
-//                    DictionaryDatabase.KEY_DEFINITION };
+//    private void showResults(String query) {
 //
-//            // Specify the corresponding layout elements where we want the columns to go
-//            int[] to = new int[] { R.id.word,
-//                    R.id.definition };
-//
-//            // Create a simple cursor adapter for the definitions and apply them to the ListView
-//            SimpleCursorAdapter words = new SimpleCursorAdapter(this,
-//                    R.layout.result, cursor, from, to);
-//            mListView.setAdapter(words);
-//
-//            // Define the on-click listener for the list items
-//            mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//
-//                @Override
-//                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                    // Build the Intent used to open WordActivity with a specific word Uri
-//                    Intent wordIntent = new Intent(getApplicationContext(), WordActivity.class);
-//                    Uri data = Uri.withAppendedPath(DictionaryProvider.CONTENT_URI,
-//                            String.valueOf(id));
-//                    wordIntent.setData(data);
-//                    startActivity(wordIntent);
-//                }
-//            });
-        }
-    }
+//        Cursor cursor = new CursorLoader(getApplicationContext(),DatabaseProvider.CONTENT_URI, null, null,
+//                new String[]{query}, null);
+//        if (cursor == null) {
+//            // There are no results
+//            //mTextView.setText(getString(R.string.no_results, new Object[]{query}));
+//        } else {
+//            // Display the number of results
+//            int count = cursor.getCount();
+//            //String countString = getResources().getQuantityString(R.plurals.search_results,
+//            //        count, new Object[] {count, query});
+//            //mTextView.setText(countString);
+//            Toast.makeText(MapsActivity.this, "WOW: You found "+count+" results!", Toast.LENGTH_SHORT).show();
+//            // Specify the columns we want to display in the result
+////            String[] from = new String[] { DictionaryDatabase.KEY_WORD,
+////                    DictionaryDatabase.KEY_DEFINITION };
+////
+////            // Specify the corresponding layout elements where we want the columns to go
+////            int[] to = new int[] { R.id.word,
+////                    R.id.definition };
+////
+////            // Create a simple cursor adapter for the definitions and apply them to the ListView
+////            SimpleCursorAdapter words = new SimpleCursorAdapter(this,
+////                    R.layout.result, cursor, from, to);
+////            mListView.setAdapter(words);
+////
+////            // Define the on-click listener for the list items
+////            mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+////
+////                @Override
+////                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+////                    // Build the Intent used to open WordActivity with a specific word Uri
+////                    Intent wordIntent = new Intent(getApplicationContext(), WordActivity.class);
+////                    Uri data = Uri.withAppendedPath(DictionaryProvider.CONTENT_URI,
+////                            String.valueOf(id));
+////                    wordIntent.setData(data);
+////                    startActivity(wordIntent);
+////                }
+////            });
+//        }
+//    }
 
     /**
      * Manipulates the map once available.
@@ -484,13 +493,12 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     @Override
-<<<<<<< HEAD
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
         String[] qry = {args.getString("QUERY")};
         switch (id) {
             case DATABASE_LOADER:
                 // Returns a new CursorLoader
-                return new CursorLoader(
+                return new android.support.v4.content.CursorLoader(
                         getApplicationContext(),   // Parent activity context
                         DatabaseProvider.CONTENT_URI,
                         null,
@@ -506,12 +514,14 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-
+        System.out.print(data.getCount());
+        Toast.makeText(MapsActivity.this, "Results: "+data.getCount(), Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
-=======
+
+    }
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putDouble("roomMarkerLongtitude", roomMarkerLongtitude);
@@ -554,7 +564,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             currentCameraLatitude = mMap.getCameraPosition().target.latitude;
             currentCameraLongtitude = mMap.getCameraPosition().target.longitude;
         }
->>>>>>> refs/remotes/origin/master
 
     }
 }
