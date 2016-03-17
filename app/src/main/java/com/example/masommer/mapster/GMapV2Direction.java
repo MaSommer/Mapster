99a28f7331abc344e67fc2990daae4f4ff1098ad
Bendik Witzoe <bendikw@gmail.com>
2016-03-16 19:51:39 -0700

    fjernet dritt

--- app/src/main/java/com/example/masommer/mapster/GMapV2Direction.java
+++ app/src/main/java/com/example/masommer/mapster/GMapV2Direction.java
@@ -24,8 +24,6 @@ import javax.xml.parsers.DocumentBuilderFactory;
  * Created by MaSommer on 07.03.16.
  */
 public class GMapV2Direction {
-    public final static String MODE_DRIVING = "driving";
-    public final static String MODE_WALKING = "walking";
 
     public GMapV2Direction() { }
 
@@ -50,62 +48,7 @@ public class GMapV2Direction {
         return null;
     }
 
-    public String getDurationText (Document doc) {
-        NodeList nl1 = doc.getElementsByTagName("duration");
-        Node node1 = nl1.item(0);
-        NodeList nl2 = node1.getChildNodes();
-        Node node2 = nl2.item(getNodeIndex(nl2, "text"));
-        Log.i("DurationText", node2.getTextContent());
-        return node2.getTextContent();
-    }
 
-    public int getDurationValue (Document doc) {
-        NodeList nl1 = doc.getElementsByTagName("duration");
-        Node node1 = nl1.item(0);
-        NodeList nl2 = node1.getChildNodes();
-        Node node2 = nl2.item(getNodeIndex(nl2, "value"));
-        Log.i("DurationValue", node2.getTextContent());
-        return Integer.parseInt(node2.getTextContent());
-    }
-
-    public String getDistanceText (Document doc) {
-        NodeList nl1 = doc.getElementsByTagName("distance");
-        Node node1 = nl1.item(0);
-        NodeList nl2 = node1.getChildNodes();
-        Node node2 = nl2.item(getNodeIndex(nl2, "text"));
-        Log.i("DistanceText", node2.getTextContent());
-        return node2.getTextContent();
-    }
-
-    public int getDistanceValue (Document doc) {
-        NodeList nl1 = doc.getElementsByTagName("distance");
-        Node node1 = nl1.item(0);
-        NodeList nl2 = node1.getChildNodes();
-        Node node2 = nl2.item(getNodeIndex(nl2, "value"));
-        Log.i("DistanceValue", node2.getTextContent());
-        return Integer.parseInt(node2.getTextContent());
-    }
-
-    public String getStartAddress (Document doc) {
-        NodeList nl1 = doc.getElementsByTagName("start_address");
-        Node node1 = nl1.item(0);
-        Log.i("StartAddress", node1.getTextContent());
-        return node1.getTextContent();
-    }
-
-    public String getEndAddress (Document doc) {
-        NodeList nl1 = doc.getElementsByTagName("end_address");
-        Node node1 = nl1.item(0);
-        Log.i("StartAddress", node1.getTextContent());
-        return node1.getTextContent();
-    }
-
-    public String getCopyRights (Document doc) {
-        NodeList nl1 = doc.getElementsByTagName("copyrights");
-        Node node1 = nl1.item(0);
-        Log.i("CopyRights", node1.getTextContent());
-        return node1.getTextContent();
-    }
 
     public ArrayList getDirection (Document doc) {
         NodeList nl1, nl2, nl3;
@@ -141,7 +84,6 @@ public class GMapV2Direction {
                 listGeopoints.add(new LatLng(lat, lng));
             }
         }
-
         return listGeopoints;
     }
 
